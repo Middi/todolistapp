0f9b5d53d05e63fdf7b3af23b08c8557428fa476
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-29 18:04:50 +0200

    port env

--- app.js
+++ app.js
@@ -132,6 +132,6 @@ app.delete('/article/:id', function(req, res){
 });
 
 // Start Server
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('server started on port 3000');
 });
