a280f0ca623b325b13f9db9c3d12832d1ab83da0
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-30 20:30:45 +0200

    changes to login

--- routes/users.js
+++ routes/users.js
@@ -10,60 +10,59 @@ router.get('/register', function(req, res){
   res.render('register');
 });
 
-// Register Process
+// Register Proccess
 router.post('/register', function(req, res){
-const name = req.body.name;
-const email = req.body.email;
-const username = req.body.username;
-const password = req.body.password;
-const password2 = req.body.password2;
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
 
-req.checkBody('name', 'Name is required').notEmpty();
-req.checkBody('email', 'Email is required').notEmpty();
-req.checkBody('email', 'Email is not valid').notEmpty();
-req.checkBody('username', 'Username is required').notEmpty();
-req.checkBody('password', 'Password is required').notEmpty();
-req.checkBody('password2', 'Password did not match').equals(req.body.password);
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Password did not match').equals(req.body.password);
 
-let errors = req.validationErrors();
+  let errors = req.validationErrors();
 
-if(errors) {
+  if(errors) {
     res.render('register', {
-        errors: errors
+      errors: errors
     });
-}
-else {
+  }
+  else {
     let newUser = new User({
-        name: name,
-        email: email,
-        username: username,
-        password:password
+      name: name,
+      email: email,
+      username: username,
+      password: password
     });
 
-    bcrypt.getSalt(10, function(err, salt){
-        bcrypt.hash(newUser.password, salt, function(err, hash){
-            if(error){
-                console.log(err);
-            }
-            else {
-                newUser.password = hash;
-                newUser.save(function(err){
-                    if(error) {
-                        console.log(err);
-                        return;
-                    }
-                    else {
-                        req.flash('success', 'you are now registered and can now log in');
-                        res.redirect('user/login');
-                    }
-                });
-            }
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          }
+          else {
+            req.flash('success','You are now registered and can log in');
+            res.redirect('/users/login');
+          }
         });
+      });
     });
-}
+  }
 });
 
 router.get('/login', function(req, res){
     res.render('login');
-})
+});
+
 module.exports = router;
